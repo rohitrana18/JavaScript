00b7bf52e99c9da5aa53723a17d320dfc06b916d
rohitrana18 <rohitrana7703@gmail.com>
2025-04-15 17:23:23 +0000

    file

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,13 @@
+/* Datatype categorize on the basis of :
+1. How you store data in memory.
+2. How you can access and retrive data from memory. */
+
+// Primitive : Call by value
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt 
+
+// Non primitive(Reference type) : Call by reference
+
+// Array , Objects , Functions
+
+// Javascript is a dynamically typed language.
