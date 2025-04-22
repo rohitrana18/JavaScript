0919eb2414e79a409430fede63a7a1921a914ee6
rohitrana18 <rohitrana7703@gmail.com>
2025-04-22 01:54:48 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,15 +6,40 @@ console.log(`My name is ${name} and my repoCount is ${repoCount}`) // know as st
 
 // Another way to create string
 
-const string = new String("Rohit") // if we run it in console it will provide key value pair as well.
-//console.log(string[3]); // for determining the key value pair.
+const string = new String("Rohit-Rana") // if we run it in console it will provide key value pair as well.
+console.log(string[3]); // for determining the key value pair.
 
 // Methods for string
 console.log(string.length); // Method to find length of a string.
-//console.log(string.toUpperCase());
-//console.log(string.toLowerCase());
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
 console.log(string.charAt(4));
 console.log(string.indexOf('h'));
+
+const Newstring= string.substring(0 , 7)
+console.log(Newstring)
+// In substring method the end of string index given by us will be not included.
+
+const anotherstring = string.slice(-8 , 6)
+console.log(anotherstring)
+// In slice method we can assign -ve  number as an index but in substring method we can't do it.
+
+const newstring = "     Rohit   " 
+console.log(newstring);
+console.log(newstring.trim())
+// We use trim to remove unnecessary space in the string to save the memory.
+
+const url = "https://Rohit.com/Rohit%1782Rana"
+console.log(url.replace('%1782', ' '));
+// Browser don't understand spaces so we use replace method to remove the unnecessary thing or to add space if needed.
+
+console.log(url.includes('Rohit'));
+// to know that the searched element present in the string or not.
+
+console.log(string.split('-'));
+// use to split a string by giving seperator or (seperator and string) both.
+// seperator can be anything like '-', ' ' 
+
 
 
 
