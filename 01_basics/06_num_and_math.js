c5b8e7f1d85da18bbe6c11a1963fb03f7cd6fb9f
rohitrana18 <rohitrana7703@gmail.com>
2025-04-22 04:23:33 +0000

    numandmath

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,14 @@
+// Simple way to define a number or this is an integer.
+const amount = 833
+console.log(amount);
+
+// Defining the number expecetally or espacially.
+const balance =new Number(199)
+console.log( balance);
+
+console.log(balance.toString())
+ // Now we can use all the string properties because it is become a string.
+ console.log(balance.toString().length)
+
+
+
